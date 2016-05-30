a4e754357f277c7bab4adc5ed97e084c972a1c23
billchen <cgb1021@gmail.com>
2016-05-30 11:55:14 +0800

    修改结束条件

--- dial.js
+++ dial.js
@@ -5,7 +5,6 @@
  * website: http://www.xhyo.com/
  * date: 2016.05.30
  *
- * require util.js
  */
 
 (function(window){
@@ -36,12 +35,21 @@ function Dial(option) {
 	// 设置高宽度和获取context
 	this.ctx = canvas.getContext('2d');
 	this.ctx.translate(this.width/2, this.height/2);
+	this.event = {};
+	if(typeof option.onready == 'function')
+		this.event.onready = option.onready.bind(this);
+	if(typeof option.onstart == 'function')
+		this.event.onstart = option.onstart.bind(this);
+	if(typeof option.onend == 'function')
+		this.event.onend = option.onend.bind(this);
+	if(typeof option.onprogress == 'function')
+		this.event.onprogress = option.onprogress.bind(this);
 
 	// 加载图片
 	this.image = new Image();
 	this.image.onload = function imgOnload(e) {
 		// 判断图片是否准备就绪
-		if(typeof option.onready == 'function')
+		if(_this.event.onready)
 			option.onready();
 		
 		_this.reset();
@@ -68,47 +76,58 @@ Dial.prototype.start = function(index) {
 	if(this.state == 2) {
 		return;
 	}
+
+	index = parseInt(index, 10)%this.length;
+
 	// console.log('start');
 	var _this = this,
-		count = 1,
-		x = -this.width/2,
-		y = -this.height/2,
-		startTime = Date.now(),
-		roundDuration = this.duration,
-		degree = 0;
+		x = -this.width/2, //开始x坐标
+		y = -this.height/2, //开始y坐标
+		startTime = Date.now(), //每圈开始时间
+		roundDuration = this.duration, //每圈转动时长
+		totalDegree = this.degree,
+		endDegree = this.round + (index +.5)/this.length; //结束角度
 
 	this.state = 2;
 
 	function draw() {
-		var time = Date.now();
+		var time = Date.now(),
+			degree = (time-startTime)/roundDuration,
+			isEnd = false;
 
-		// console.log(count, _this.round, index, _this.index)
-		if((count == _this.round && index === _this.index) || _this.state == 1) {
-			// 动画结束条件
-			_this.state = 1;
-			_this.degree = degree;
-			// console.log(_this.degree);
-			return;
-		}
-
-		degree = (time-startTime)/roundDuration;
+		//每一圈结束条件
 		if(degree >= 1) {
 			startTime = time;
-			roundDuration = (1+count*_this.durationStep)*_this.duration;
-			count++;
-			degree = degree-1;
-			// console.log(count,roundDuration)
+			roundDuration = (1+_this.durationStep)*roundDuration;
+			totalDegree += 1;
+			degree = totalDegree;
+			// console.log(round,roundDuration)
+		} else
+			degree = totalDegree + degree;
+
+		if(degree >= endDegree || _this.state == 1) {
+			// 动画结束条件
+			_this.state = 1;
+			degree = _this.degree = endDegree%1;
+			//console.log(degree , endDegree, _this.index, index);
+			if(_this.event.onend)
+				_this.event.onend();
+			isEnd = true;
+			// console.log(_this.degree);
 		}
-		degree = (_this.degree+degree)%1;
-		_this.index = Math.floor(degree*_this.length);
+
+		_this.index = Math.floor((degree%1)*_this.length);
 		// 开始画指针
 		_this.ctx.save();
 		_this.ctx.clearRect(x, y, _this.width, _this.height);
 		_this.ctx.drawImage(_this.image, 0, 0, _this.image.width/2, _this.image.height, x, y, _this.width, _this.height);
-		_this.ctx.rotate(degree*Math.PI*2);
+		_this.ctx.rotate((degree%1)*Math.PI*2);
 		_this.ctx.drawImage(_this.image, _this.image.width/2, 0, _this.image.width/2, _this.image.height, x, y, _this.width, _this.height);
 		_this.ctx.restore();
-		window.requestAnimationFrame(arguments.callee);
+
+		// console.log(round , _this.round , degree , endDegree);
+		if(!isEnd)
+			window.requestAnimationFrame(arguments.callee);
 	}
 	draw();
 };
