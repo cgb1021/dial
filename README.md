4cd34ea80df00d6b5cb63811b0634a35a457759d
billchen <cgb1021@gmail.com>
2016-06-03 14:43:39 +0800

    支持转盘转动

--- README.md
+++ README.md
@@ -1,40 +1,49 @@
 # dial
 纯h5 转盘游戏
+author: billchen(48838096@qq.com)
+website: http://www.xhyo.com/
+date: 2016.05.30
 
 初始化参数说明
 
-option {canvasId:"dial", //canvas id
+    option {canvasId:"dial", //canvas id
  			duration: 500, //每一圈运行时长
- 			round: 5, //运行圈数
+ 			rounds: 5, //运行圈数
  			length: 6, //奖品区间数量
  			degree: 0, //初始化角度
  			width: documentWidth*0.828, //canvas 宽度
  			height: documentWidth*0.828, //canvas 高度
- 			image: "dial.png", //转盘UI背景图
+ 			image: "dial.png", //转盘UI背景图 雪碧图中，第一个位置是指针（要求是正方形），第二个位置是背景
+ 			reverse: false, //默认为false（转动指针的情况）。当转动对象是转盘时，请设置为true
  			onready: function() {}, //初始化完毕事件（图片加载完毕）
- 			onroundend: function() {this.roundDuration = (1+.5)*this.roundDuration;}, //每一圈执行完毕
+ 			onroundend: function() {if(this.finishDegree > -1) {this.currentDuration = (1+.5)*this.currentDuration;}}, //每一圈执行完毕
  			onend: function() {} //整个动画执行完毕
  			}
- 
- 对象属性说明
- 
+			
+对象属性说明
+
+   animationId:0 //定时器id
+   counter: 0 //已经完成的圈数
    ctx: CanvasRenderingContext2D //canvas context
-   degree: 0 //初始化角度
-   duration: 500 //初始化每圈时长
+   currentDuration: 0 //当前圈的执行时长
+   degree: 0 //当前角度
+   duration: 500 //初始化时每圈时长
    event: Object //事件 onready,onend,onroundend
+   startDegree: 初始化角度
+   finishDegree:0 //结束角度（>-1才为有效结束角度）
    height: 310.5 //canvas 高度
-   image: img //转盘UI背景图
+   image: img //转盘UI图（image对象）
    index: 0 //当前指针指向区间
    length: 6 //区间个数（礼物个数）
-   round: 5 //转动圈数
-   roundDuration: 0 //每圈执行时长
-   state: 1 //状态，0,未初始化;1,初始化完毕;2,执行中
+   rounds: 5 //转动圈数
+   state: 1 //运行状态 0:未初始化(UI图未加载完毕),1:正常状态(初始化完毕),2:运行中(转动中),3:准备停止(调用stop方法)
    width:310.5 //canvas 宽度
+   reverse: false 是否反转角度
 
- 对象方法说明
- 
-   start(index) //开始转动,index结束的区间
-   stop() //手动停止
+对象方法
+
+   start(index) //开始转动,index结束的区间（选）。不传参数，将开启无限模式
+   stop(index) //手动停止, index结束的区间（选）
    reset() //重置
    
-   使用问题请联系48838096@qq.com
+使用问题请联系48838096@qq.com
--- dial.js
+++ dial.js
@@ -7,33 +7,36 @@
  *
  * 初始化参数说明
  * option {canvasId:"dial", //canvas id
- 			duration: 500, //每一圈运行时长
- 			rounds: 5, //运行圈数
- 			length: 6, //奖品区间数量
- 			degree: 0, //初始化角度
- 			width: documentWidth*0.828, //canvas 宽度
- 			height: documentWidth*0.828, //canvas 高度
- 			image: "dial.png", //转盘UI背景图 雪碧图中，第一个位置是指针（要求是正方形），第二个位置是背景
- 			onready: function() {}, //初始化完毕事件（图片加载完毕）
- 			onroundend: function() {this.currentDuration = (1+.5)*this.currentDuration;}, //每一圈执行完毕
- 			onend: function() {} //整个动画执行完毕
- 			}
+   duration: 500, //每一圈运行时长
+   rounds: 5, //运行圈数
+   length: 6, //奖品区间数量
+   degree: 0, //初始化角度
+   width: documentWidth*0.828, //canvas 宽度
+   height: documentWidth*0.828, //canvas 高度
+   image: "dial.png", //转盘UI背景图 雪碧图中，第一个位置是指针（要求是正方形），第二个位置是背景
+   reverse: false, //默认为false（转动指针的情况）。当转动对象是转盘时，请设置为true
+   onready: function() {}, //初始化完毕事件（图片加载完毕）
+   onroundend: function() {if(this.finishDegree > -1) {this.currentDuration = (1+.5)*this.currentDuration;}}, //每一圈执行完毕
+   onend: function() {} //整个动画执行完毕
+ }
  * 对象属性说明
- * counter: 0 //已经完成的圈数
+ * animationId:0 //定时器id
+   counter: 0 //已经完成的圈数
    ctx: CanvasRenderingContext2D //canvas context
    currentDuration: 0 //当前圈的执行时长
-   degree: 0 //初始化角度
-   duration: 500 //初始化每圈时长
+   degree: 0 //当前角度
+   duration: 500 //初始化时每圈时长
    event: Object //事件 onready,onend,onroundend
    startDegree: 初始化角度
-   finishDegree:0 //结束角度 >-1才为有效结束角度
+   finishDegree:0 //结束角度（>-1才为有效结束角度）
    height: 310.5 //canvas 高度
-   image: img //转盘UI背景图
+   image: img //转盘UI图（image对象）
    index: 0 //当前指针指向区间
    length: 6 //区间个数（礼物个数）
    rounds: 5 //转动圈数
-   state: 1 //状态，0,未初始化;1,初始化完毕;2,执行中
+   state: 1 //运行状态 0:未初始化(UI图未加载完毕),1:正常状态(初始化完毕),2:运行中(转动中),3:准备停止(调用stop方法)
    width:310.5 //canvas 宽度
+   reverse: false 是否反转角度
  *
  * 对象方法
  * start(index) //开始转动,index结束的区间（选）。不传参数，将开启无限模式
@@ -121,7 +124,7 @@ function Dial(option) {
 * 重置
 */
 Dial.prototype.reset = function() {
-	this.state = 1; //运行状态 0:未初始化,1:正常状态(初始化完毕),2:运行中(转动中),3:准备停止(调用stop方法)
+	this.state = 1;
 	if(this.animationId)
 		window.cancelAnimationFrame(this.animationId);
 	//重置属性
@@ -161,7 +164,7 @@ Dial.prototype.start = function(index) {
 
 	if(index != undefined) {
 		//计算结束角度
-		this.finishDegree = this.rounds + (index%this.length +.5)/this.length;
+		this.finishDegree = this.rounds + (this.reverse ? 1-(index%this.length +.5)/this.length : (index%this.length +.5)/this.length);
 		//console.log('this.finishDegree', this.finishDegree)
 	} else {
 		this.finishDegree = -1;
@@ -226,9 +229,9 @@ Dial.prototype.stop = function(index) {
 	this.state = 3;
 	if(index != undefined) {
 		//计算结束角度
-		this.finishDegree = this.counter + this.rounds + (index%this.length +.5)/this.length;
+		this.finishDegree = this.counter + this.rounds + (this.reverse ? 1-(index%this.length +.5)/this.length : (index%this.length +.5)/this.length);
 	} else {
-		this.finishDegree = (this.index +.5)/this.length;
+		this.finishDegree = this.reverse ? 1-(this.index +.5)/this.length : (this.index +.5)/this.length;
 	}
 };
 
